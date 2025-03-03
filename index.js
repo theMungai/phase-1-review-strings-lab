6cecbf41c7607dbf2edf2e8773ae3f8aabe71d98
theMungai <mungaithesimon@gmail.com>
2025-03-03 15:19:26 +0300

    Strings Lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser = "George";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+const shortGreeting = `Welcome,  ${currentUser[0]}!`
